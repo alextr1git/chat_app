877afbd53fe569747054056b8543d135534ac7ee
Aliaksandr Ivashkevich <101880677+alextr1git@users.noreply.github.com>
2024-01-09 16:08:39 +0300

    added logout

---
+++ navigation/lib/app_router/app_router_guard.dart
@@ -0,0 +1,24 @@
+/*
+import '../navigation.dart';
+
+class AuthGuard extends AutoRouteGuard {
+  @override
+  void onNavigation(NavigationResolver resolver, StackRouter router) {
+    // the navigation is paused until resolver.next() is called with either
+    // true to resume/continue navigation or false to abort navigation
+    if (authenticated) {
+      // if user is authenticated we continue
+      resolver.next(true);
+    } else {
+      // we redirect the user to our login page
+      // tip: use resolver.redirect to have the redirected route
+      // automatically removed from the stack when the resolver is completed
+      resolver.redirect(LoginRoute(onResult: (success) {
+        // if success == true the navigation will be resumed
+        // else it will be aborted
+        resolver.next(success);
+      }));
+    }
+  }
+}
+*/
