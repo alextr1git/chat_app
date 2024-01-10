af708ab1c9ece72284ae2f110e7ac69b77d4faec
Aliaksandr Ivashkevich <101880677+alextr1git@users.noreply.github.com>
2024-01-10 15:43:50 +0300

    created account settings view

--- core/lib/di/data_di.dart
+++ core/lib/di/data_di.dart
@@ -2,12 +2,10 @@ import 'package:data/data.dart';
 import 'package:data/providers/auth/authentication_provider.dart';
 import 'package:data/repositories/user/user_repository_impl.dart';
 import 'package:domain/domain.dart';
-import 'package:domain/usecases/auth_usecases/check_user_auth_usecase.dart';
-import 'package:domain/usecases/auth_usecases/login_usecase.dart';
-import 'package:domain/usecases/auth_usecases/register_usecase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:core/config/firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 import 'app_di.dart';
 
@@ -15,10 +13,12 @@ final DataDI dataDI = DataDI();
 
 class DataDI {
   late final FirebaseAuth _firebaseAuth;
+  late final FirebaseDatabase _firebaseDatabase;
 
   Future<void> initDependencies() async {
     await _initFirebase();
     _firebaseAuth = _initFirebaseAuth();
+    _firebaseDatabase = _initFirebaseDatabase();
     _initAuthResources();
   }
 
@@ -29,7 +29,10 @@ class DataDI {
   }
 
   FirebaseAuth _initFirebaseAuth() => FirebaseAuth.instance;
+  FirebaseDatabase _initFirebaseDatabase() => FirebaseDatabase.instance;
+
   FirebaseAuth get firebaseAuth => _firebaseAuth;
+  FirebaseDatabase get firebaseDatabase => _firebaseDatabase;
 
   void _initAuthResources() {
     appLocator.registerLazySingleton<AuthenticationProvider>(
@@ -54,6 +57,24 @@ class DataDI {
 
     appLocator.registerLazySingleton<CheckUserAuthenticationUseCase>(
       () => CheckUserAuthenticationUseCase(
+        userRepository: appLocator.get<UserRepository>(),
+      ),
+    );
+
+    appLocator.registerLazySingleton<SendVerificationEmailUseCase>(
+      () => SendVerificationEmailUseCase(
+        userRepository: appLocator.get<UserRepository>(),
+      ),
+    );
+
+    appLocator.registerLazySingleton<LogoutUserUseCase>(
+      () => LogoutUserUseCase(
+        userRepository: appLocator.get<UserRepository>(),
+      ),
+    );
+
+    appLocator.registerLazySingleton<GetUserUseCase>(
+      () => GetUserUseCase(
         userRepository: appLocator.get<UserRepository>(),
       ),
     );
